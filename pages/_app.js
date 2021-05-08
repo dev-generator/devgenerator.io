4c1212dbd1cd4930f91067dbe35ec37b8c951da1
Christopher Pezza <chris.pezza@hey.com>
2021-05-08 15:52:38 -0400

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
 import '../styles/globals.css'
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: '#22D3EE',
+})
+
+if (typeof window !== 'undefined') {
+  progress.start()
+  progress.finish()
+}
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />
