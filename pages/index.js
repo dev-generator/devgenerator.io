c2d0df5b3be3c5ef5c9ef92261ddf81036ffac3c
Christopher Pezza <chris.pezza@hey.com>
2021-05-13 18:30:08 -0400

    meta tag updates

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,7 @@ export default function Home() {
       <main>
         <Head>
           <title>DevGenerator: Generate Config files for the likes of Rubocop, Package.json</title>
-          <meta
-            name="description"
-            content="Generate files for various linters, base files, etc. without having to manually create or remember every available input that has to go into a file."
-          />
-          <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <Hero />
         <Team />
         <Features />
