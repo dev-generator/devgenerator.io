9cb470a5217a1f10551756ec9dc994bf9111bc3d
Christopher Pezza <chris.pezza@hey.com>
2021-05-16 15:51:52 -0400

    fix file route path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['.src/pages/**/*.js', '.src/components/**/*.js'],
+  purge: ['./src/pages/**/*.js', './src/components/**/*.js'],
   darkMode: false,
   theme: {
     colors: {
